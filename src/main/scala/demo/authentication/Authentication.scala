b3641202ed0b801b9f1983239eb5ce1cedaee0d3
jean yang writeme <jean.yang.writeme@gmail.com>
2013-03-15 21:11:16 -0400

    some documentation

--- src/main/scala/demo/authentication/Authentication.scala
+++ src/main/scala/demo/authentication/Authentication.scala
@@ -5,20 +5,24 @@ import cap.jeeveslib.ast.JeevesTypes._
 import cap.jeeveslib.jeeves._
 
 /**
-* Basic authentication.
+* Authentication example.
 * @author jeanyang
 */
 class Principal extends Atom
 case class User(id: BigInt, name: String, private val _pwd: String = "")
   (implicit jlib: JeevesLib[Cred]) extends Principal {
-  // Initial password.
+  // Initial password: the initial password is a sensitive value that only
+  // the current user can see.
   private val pwdLabel = jlib.mkLabel()
   jlib.restrict(pwdLabel
     , (ctxt: ObjectExpr[Cred]) =>
     ctxt.p~'id === id && ctxt.p.password === password)
   var password: ObjectExpr[S] = jlib.mkSensitive(pwdLabel, S(_pwd), S(""))
 
-    // Password update.
+  // Password update: the password is protected by a write policy for integrity
+  // that says only the current user can update the password value.
+  // This policy also encapsulates the confidentiality that only the current
+  // user can see the result of the update.
   private var pwdRef = ProtectedObjectRef[Cred, Cred](password
     , ictxt => octxt =>
     (ictxt.p~'id === id) && (ictxt.p.password === password)
@@ -43,7 +47,8 @@ case class Cred(p: Principal) extends Atom
 
 object Authentication extends JeevesLib[Cred] {
   val admin = Admin()(this)
-  
+
+  // File class where the contents is a protected reference cell.
   class File(val owner: Principal, private val _contents: String = "")
   (implicit jlib: JeevesLib[Cred]) extends Atom {
     private val contents = ProtectedObjectRef[Cred, Cred](S(_contents)
@@ -54,6 +59,7 @@ object Authentication extends JeevesLib[Cred] {
     def getContents(): ObjectExpr[S] = contents.v.asInstanceOf[ObjectExpr[S]]
   }
 
+  // No declassication is needed for the login function!
   def login (p: Principal, pwd: String): ObjectExpr[Cred] = {
     p match {
       case u:User =>
