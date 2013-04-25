baa3433a020e020853755fdc5997c0e848d6f717
jean yang writeme <jean.yang.writeme@gmail.com>
2013-04-24 20:20:20 -0400

    more tests for the dodonor example for Jeeves DB

--- src/test/scala/jeeves/jeevesdb/sorting/DonorAmounts.scala
+++ src/test/scala/jeeves/jeevesdb/sorting/DonorAmounts.scala
@@ -8,25 +8,49 @@ import org.squeryl._
 import adapters.H2Adapter
 import PrimitiveTypeMode._
 
-import Tables._
-
 class DatabaseUnitSpec extends Specification with BeforeExample{
   def before = initialize
 
   sequential
 
+  val donor1 = new DonorRecord("donor1@donor.org", 100, 0, "x")
+  val donor2 = new DonorRecord("donor2@donor.org", 200, 0, "y")
+  val donor3 = new DonorRecord("donor3@donor.org", 300, 0, "x")
+
   "Schema stored in database should" >> {
     "allow creation of new entries" >> {
       transaction {
-        val newDonor = donors.insert(new DonorRecord("donor1@donor.org", 100))
+        val newDonor =
+          Tables.writeDonor(donor1)
           newDonor.id must_== 1
       }
     }
     "allow retrieval of entries" >> {
       transaction {
-        donors.insert(new DonorRecord("donor2@donor.org", 200))
-        val lookupDonor = donors.where(a => a.amount === 200).single
+        Tables.writeDonor(donor2)
+        val lookupDonor = Tables.donors.where(a => a.amountHigh === 200).single
         lookupDonor.id must_== 1
+      }
+    }
+    "find distinct labels when labels are distinct" >> {
+      transaction {
+        Tables.writeDonor(donor1)
+        Tables.writeDonor(donor2)
+        val labels = Tables.getDistinctLabels()
+        labels.length must_==2
+        labels.exists(_ == "x") must_==true
+        labels.exists(_ == "y") must_==true
+      }
+    }
+    "find distinct labels when labels are not distinct" >> {
+      transaction {
+        Tables.writeDonor(donor1)
+        Tables.writeDonor(donor2)
+        Tables.writeDonor(donor3)
+        val labels = Tables.getDistinctLabels()
+        labels.length must_==2
+        labels.exists(_ == "x") must_==true
+        labels.exists(_ == "y") must_==true
       }
     }
   }
@@ -44,7 +68,7 @@ class DatabaseUnitSpec extends Specification with BeforeExample{
     inTransaction {
       Tables.drop
       Tables.create
-      printDdl
+      Tables.printDdl
     }
   }
 } 
--- src/test/scala/jeeves/jeevesdb/sorting/Tables.scala
+++ src/test/scala/jeeves/jeevesdb/sorting/Tables.scala
@@ -3,22 +3,24 @@
  */
 package test.cap.jeeveslib.jeeves.jeevesdb.sorting
 
-//import JConfBackend._
-
+import org.squeryl._
 import org.squeryl.KeyedEntity
 import org.squeryl.adapters.MySQLAdapter
 import org.squeryl.PrimitiveTypeMode._
-// import org.squeryl.customtypes.CustomTypesMode._
-// import org.squeryl.customtypes._
 import org.squeryl.Schema
 import org.squeryl.Session
 import org.squeryl.SessionFactory
 
+// TODO: Obviously we will have to build infrastructure on top of this to
+// generate some of this automatically...
+
 class DonorRecord(
     val email: String
-  , var amount: Int
+  , var amountHigh: Int
+  , var amountLow: Int
+  , var amountLabel: String
   ) extends KeyedEntity[Int] {
-  def this() = this("", -1)
+  def this() = this("", -1, -1, "")
   val id = 0;
 }
 
@@ -29,7 +31,24 @@ object Tables extends Schema {
       u.id        is(autoIncremented)
     , u.email     is(unique)
   ))
-  def writeDonor(donorRecord: DonorRecord): Unit = {
+  def writeDonor(donorRecord: DonorRecord): DonorRecord = {
     transaction { donors.insert(donorRecord) }
   }
+
+  def getDistinctLabels(): List[String] = {
+    val labels = transaction {
+      from(donors)(s => select(s.amountLabel))
+    }
+    labels.iterator.toList.distinct
+  }
+
+  def generateLabelViews(): List[Queryable[DonorRecord]] = {
+    ???
+  }
+
+  def sortDonorsByAmount(): Queryable[DonorRecord] = {
+    ???
+  }
+
+  // TODO: Think of way to store policies with the database.
 }
